6449ead24092ef3d232e07dab786d5404e313f78
Nicholas Vincent <nickmvincent@gmail.com>
2019-12-09 14:55:03 -0600

    new comparative plotter (compare search engines)

--- plotter.py
+++ plotter.py
@@ -5,50 +5,22 @@ import matplotlib
 import json
 import pandas as pd
 import numpy as np
-
-
-with open('examples/results/procon_munich.json', 'r', encoding='utf8') as f:
-    d = json.load(f)
-
-#%%
-d.keys()
-#%%
-all_links = []
-    
-page_num = 1
-for query in d.keys():
-    links = d[query]['1']
-    print(links)
-    for link in links:
-        print(link)
-        link['query'] = query
-    all_links += links
-
-df = pd.DataFrame(all_links)
-
-df['width'] = df.right - df.left
-df['height'] = df.bottom - df.top
-
-#%%
-df.head()
+from urllib.parse import urlparse
+import seaborn as sns
 
 
 #%%
-df.head()
+device = 'desktop'
+search_engines = [
+    'google',
+    'duckduckgo',
+]
+queries = 'trend_sample'
 
 
-#%%
 full_width = 10
 full_height = 8
-#%%
-for key in ['width', 'left', 'right']:
-    df['norm_{}'.format(key)] = df[key] / df['right'].max()
 
-for key in ['height', 'top', 'bottom']:
-    df['norm_{}'.format(key)] = df[key] / df['bottom'].max()
-
-#%%
-from urllib.parse import urlparse
 def extract(x):
     domain = urlparse(x.href).netloc
     try:
@@ -57,96 +29,102 @@ def extract(x):
         ret = domain
     return ret
 
-df['domain'] = df.apply(extract, axis=1)
-df['platform_ugc'] = df['domain'].str.contains('|'.join(
-    ['wikipedia', 'twitter', 'facebook']
-))
-df['wikipedia_appears'] = df['domain'].str.contains('wikipedia')
-df.head()
 
 #%%
-df[df.domain == 'en.wikipedia']
-#%%
-# font = {'family' : 'normal',
-#         'weight' : 'bold',
-#         'size'   : 8}
+for search_engine in search_engines:
+    k = f'{device}_{search_engine}_{queries}'
 
-#matplotlib.rc('font', **font)
-#print(d)
-fig,ax = plt.subplots(1, 1, figsize=(full_width, full_height))
-plt.gca().invert_yaxis()
+    with open(f'output/{device}/{search_engine}/{queries}/results.json', 'r', encoding='utf8') as f:
+        d = json.load(f)
+    all_links = []
+        
+    for query in d.keys():
+        links = d[query]['1_xy']
+        #print(links)
+        for link in links:
+            #print(link)
+            link['query'] = query
+        all_links += links
 
+    df = pd.DataFrame(all_links)
 
-for i_row, row in df.iterrows():
-    if row.width == 0:
-        continue
+    df['width'] = df.right - df.left
+    df['height'] = df.bottom - df.top
+    print(df.head(3))
 
-    x = row['norm_left']
-    y = row['norm_bottom']
-    width = row['norm_width']
-    height = row['norm_height']
-    domain = row['domain']
+    for key in ['width', 'left', 'right']:
+        df['norm_{}'.format(key)] = df[key] / df['right'].max()
 
-    rect = matplotlib.patches.Rectangle((x,y),width,height,linewidth=1,edgecolor='g',facecolor='none')
-    if row['platform_ugc']:
-        color = 'b'
-    elif 'google' in domain:
-        color = 'lightgray'
-    else:
-        color = 'grey'
-    plt.annotate(domain, (x, y), color=color)
-    # Add the patch to the Axes
-    ax.add_patch(rect)
+    for key in ['height', 'top', 'bottom']:
+        df['norm_{}'.format(key)] = df[key] / df['bottom'].max()
 
-#plt.show()
-plt.savefig('overlaid.png')
+    df['domain'] = df.apply(extract, axis=1)
+    df['platform_ugc'] = df['domain'].str.contains('|'.join(
+        ['wikipedia', 'twitter', 'facebook', 'instagram', 'reddit', ]
+    ))
+    df['wikipedia_appears'] = df['domain'].str.contains('wikipedia')
 
-#%%
-roundto = 1
-df['grid_left'] = np.round(df['norm_left'], roundto)
-df['grid_bottom'] = np.round(df['norm_bottom'], roundto)
-df['grid_width'] = np.round(df['norm_width'], roundto)
-df['grid_height'] = np.round(df['norm_height'], roundto)
-df.head()
-
-# heatmap_points = np.zeros((11, 1))
+    fig,ax = plt.subplots(1, 1, figsize=(full_width, full_height))
+    plt.gca().invert_yaxis()
 
 
+    for i_row, row in df.iterrows():
+        if row.width == 0:
+            continue
 
-# for ix, x in enumerate(np.linspace(0, 1, num=11)):
-#     for iy, y in enumerate(np.linspace(0, 1, num=11)):
-#         print(y, x)
-#         try:
-#             heatmap_points[iy, ix] = tmp.loc[y, x]
-#         except KeyError:
-#             heatmap_points[iy, ix] = 0
+        x = row['norm_left']
+        y = row['norm_bottom']
+        width = row['norm_width']
+        height = row['norm_height']
+        domain = row['domain']
 
-#%%
+        rect = matplotlib.patches.Rectangle((x,y),width,height,linewidth=1,edgecolor='g',facecolor='none')
+        if row['platform_ugc']:
+            color = 'b'
+        elif 'google' in domain:
+            color = 'lightgray'
+        else:
+            color = 'grey'
+        plt.annotate(domain, (x, y), color=color)
+        # Add the patch to the Axes
+        ax.add_patch(rect)
 
-#%%
-df.groupby(['grid_left', 'grid_bottom']).wikipedia_appears.mean().unstack(level=0).fillna(0)
+    plt.savefig(f'reports/{k}_overlaid.png')
 
-#%%
-heatmap_points = np.zeros((11, 11))
-#%%
-tmp = df.groupby(['grid_left', 'grid_bottom']).wikipedia_appears.mean().unstack(level=0).fillna(0)
-for ix, x in enumerate(np.linspace(0, 1, num=11)):
-    for iy, y in enumerate(np.linspace(0, 1, num=11)):
-        print(y, x)
-        try:
-            heatmap_points[iy, ix] = tmp.loc[y, x]
-        except KeyError:
-            heatmap_points[iy, ix] = 0
+    roundto = 1
+    df['grid_left'] = np.round(df['norm_left'], roundto)
+    df['grid_bottom'] = np.round(df['norm_bottom'], roundto)
+    df['grid_width'] = np.round(df['norm_width'], roundto)
+    df['grid_height'] = np.round(df['norm_height'], roundto)
+    print(df.head(3))
 
-#%%
-import seaborn as sns
-sns.heatmap(heatmap_points)
-plt.savefig('heatmap')
+    gridded = df[df.wikipedia_appears == True].groupby(['grid_left', 'grid_bottom']).wikipedia_appears.sum().unstack(level=0).fillna(0)
+    num_queries = len(set(df['query']))
+    print('num_queries', num_queries)
+    heatmap_points = np.zeros((11, 11))
 
-#%%
-df.groupby('query').wikipedia_appears.agg(any)
+    for ix in range(0, 11):
+        x = np.round(ix * 0.1, 1)
+        for iy in range(0, 11):
+            y = np.round(iy * 0.1, 1)
+            #print(y, x)
+            try:
+                heatmap_points[iy, ix] = gridded.loc[y, x] / num_queries
+            except KeyError:
+                heatmap_points[iy, ix] = 0
 
-#%%
-df[df.norm_top < 0.20].groupby('query').wikipedia_appears.agg(any)
 
-#%%
+    hfig, ax = plt.subplots(1, 1)
+    hmap = sns.heatmap(heatmap_points, ax=ax)
+    hfig.savefig(f'reports/{k}_heatmap.png')
+    print(search_engine)
+    print(df.groupby('query').wikipedia_appears.agg(any))
+
+    print('The incidence rate is')
+    print(df.groupby('query').wikipedia_appears.agg(any).mean())
+
+    print('The top-quarter incidence rate is')
+    print(df[df.grid_bottom <= 0.25].groupby('query').wikipedia_appears.agg(any).mean())
+
+
+# %%
