15b2ce60adb256b83011d789cc1f792d4a6ed678
Nicholas Vincent <nickmvincent@gmail.com>
2020-02-15 11:22:19 -0600

    errs4

--- errs.sh
+++ errs.sh
@@ -1,8 +1,5 @@
-/usr/bin/time -v node driver.js desktop google errs3_desktop_google_trend &> logs/errs3_desktop_google_trend.txt
-/usr/bin/time -v node driver.js desktop bing errs3_desktop_bing_trend &> logs/errs3_desktop_bing_trend.txt
-/usr/bin/time -v node driver.js desktop duckduckgo errs3_desktop_duckduckgo_trend &> logs/errs3_desktop_duckduckgo_trend.txt
-/usr/bin/time -v node driver.js mobile google errs3_mobile_google_top &> logs/errs3_mobile_google_top.txt
-/usr/bin/time -v node driver.js mobile google errs3_mobile_google_trend &> logs/errs3_mobile_google_trend.txt
-/usr/bin/time -v node driver.js mobile bing errs3_mobile_bing_top &> logs/errs3_mobile_bing_top.txt
-/usr/bin/time -v node driver.js mobile bing errs3_mobile_bing_trend &> logs/errs3_mobile_bing_trend.txt
-/usr/bin/time -v node driver.js mobile duckduckgo errs3_mobile_duckduckgo_trend &> logs/errs3_mobile_duckduckgo_trend.txt
+#/usr/bin/time -v node driver.js desktop bing errs4_desktop_bing_trend &> logs/errs4_desktop_bing_trend.txt
+/usr/bin/time -v node driver.js desktop duckduckgo errs4_desktop_duckduckgo_trend &> logs/errs4_desktop_duckduckgo_trend.txt
+#/usr/bin/time -v node driver.js mobile google errs4_mobile_google_top &> logs/errs4_mobile_google_top.txt
+#/usr/bin/time -v node driver.js mobile bing errs4_mobile_bing_top &> logs/errs4_mobile_bing_top.txt
+/usr/bin/time -v node driver.js mobile duckduckgo errs4_mobile_duckduckgo_trend &> logs/errs4_mobile_duckduckgo_trend.txt
